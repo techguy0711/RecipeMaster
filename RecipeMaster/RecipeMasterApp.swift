1c32c4496776182a9a0e65c462fb1685bf586fdd
Kristhian De Oliveira <kristhian.deoliveira@gmail.com>
2025-01-15 08:53:15 -0500

    Initial Commit

---
+++ RecipeMaster/RecipeMasterApp.swift
@@ -0,0 +1,32 @@
+//
+//  RecipeMasterApp.swift
+//  RecipeMaster
+//
+//  Created by Kristhian De Oliveira on 1/15/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RecipeMasterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
